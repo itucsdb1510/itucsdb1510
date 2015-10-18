3426b9b401c09dc05cde02176ff412ddec485f8f
mytk91 <tekpinar@itu.edu.tr>
2015-10-18 23:46:46 +0300

    adde scores.html and trails.html

--- server.py
+++ server.py
@@ -24,6 +24,16 @@ def topics_page():
     now = datetime.datetime.now();
     return render_template('topics.html', current_time=now.ctime())
 
+@app.route('/trails')
+def trails_page():
+    now = datetime.datetime.now();
+    return render_template('trails.html', current_time=now.ctime())
+
+@app.route('/scores')
+def scores_page():
+    now = datetime.datetime.now();
+    return render_template('scores.html', current_time=now.ctime())
+
 if __name__ == '__main__':
     PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
     app.run(host='0.0.0.0', port=int(PORT))
