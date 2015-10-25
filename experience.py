22ee4b986f2b4e697e7d31cdc52271d5bb512743
mytk91 <tekpinar@itu.edu.tr>
2015-10-25 12:40:48 +0300

    Experience class defined

---
+++ experience.py
@@ -0,0 +1,9 @@
+class Experience:
+    def __init__(self, title, username, start, finish, period):
+        self.title=title
+        self.username=username
+        self.start=start
+        self.finish=finish
+        self.period=period
+        
+        
