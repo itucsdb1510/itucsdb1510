c14c61a92860de6fae7763a80ff46958a3b60a01
mytk91 <tekpinar@itu.edu.tr>
2015-10-28 19:16:30 +0300

    added  functions of experience class

--- store.py
+++ store.py
@@ -1,7 +1,11 @@
+
+
 class Store:
     def __init__(self):
         self.teams = {}
         self.last_key = 0
+        self.experiences= {}
+        self.exp_key = 0
 
     def add_team(self, team):
         self.last_key += 1
@@ -19,3 +23,40 @@ class Store:
 
     def addMember(self, key):
         self.teams[key].member_count = 5
+        
+        
+        
+     ########################EXPERINCES###########################   
+        
+        
+        
+        
+    def add_experience(self, team):
+        self.exp_key += 1
+        self.experiences[self.exp_key] = experience
+        
+    def delete_experience(self, key):
+        del self.experiences[key]
+        self.exp_key -= 1
+        
+    def get_experience(self, key):
+        return self.experiences[key]
+    
+    def get_experiences(self):
+        return sorted(self.experiences.items())
+    
+    
+    def update_experience(self, key, title, username, start, finish, period):
+       self.experiences[key].title = title
+       self.experiences[key].username=username
+       self.experiences[key].start=start
+       self.experiences[key].finish=finish
+       self.experiences[key].period=period
+
+
+
+
+
+    
+    
+    
